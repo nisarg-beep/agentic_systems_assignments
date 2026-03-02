094a9431e3c46d5cbde7e28b94788575788cd98c
Khushi Jain <khushijain@Khushis-MacBook-Air.local>
2026-03-02 16:26:36 +0530

    solution to the first part

---
+++ python-essentials-1/part1.py
@@ -0,0 +1,18 @@
+x = int(input("enter the first number:"))
+
+if x == str:
+    raise ValueError("invalid ")
+
+y = int(input("enter the second number:"))
+
+if y == str:
+    raise ValueError("invalid")
+
+sum = x + y 
+print ("The sum of the given numbers is :" ,sum)
+
+
+if y == 0:
+    raise ZeroDivisionError("cannnot divide with zero")
+div = x / y 
+print (div )
